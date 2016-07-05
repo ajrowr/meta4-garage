0b14bf465a1224c073a4e118c1ae5ab225c164dd
Alan Rowarth <alan@codex.cx>
2016-07-04 18:41:57 -0700

    Tidyup

--- objloader/scene.js
+++ objloader/scene.js
@@ -188,108 +188,17 @@ window.ExperimentalScene = (function () {
         
         console.log('setting up');
         
-        
-        var cfgs = {
-            n1: {
-                pos: {x:0.5, y:0, z:1.8},
-                sz: {scale:0.01},
-                rotate: true
-            },
-            n2: {
-                sz: {scale:0.002},
-                pos: {x:0.0, y:0.0, z:3}
-            },
-            n3: {
-                pos: {x:0.5, y:0, z:2.0},
-                rotate: true
-                
-            },
-            n3n: {
-                pos: {x:0.5, y:0, z:2.0},
-                rotate: true
-                
-            },
-            n4: {
-                sz: {scale:0.01},
-                pos: {x:0.5, y:0, z:2.8},
-                rotate: true
-            },
-            n5: {
-                rotate: true,
-                pos: {x:0, y:0, z:2.75},
-                sz: {scale: 0.016}
-            },
-            n6: {
-                rotate: true,
-                pos: {x:0, y:0, z:2.75},
-                sz: {scale: 0.012}
-            },
-            m1: {
-                sz: {scale: 0.2},
-                pos: {x:0, y:1, z:2},
-                rotate: true
-            },
-            n9: {
-                sz: {scale:0.01},
-                pos: {x:0.5, y:0, z:2.8},
-                rotate: true
-            },
-            n14: {
-                sz: {scale:0.002},
-                pos: {x:0.5, y:-1, z:2.0},
-                rotate: true,
-                meshLimit: 366780
-            },
-            n14a: {
-                sz: {scale:0.001},
-                pos: {x:0.5, y:0, z:2.0},
-                rotate: true
-            },
-            n16: {
-                sz: {scale:0.01},
-                pos: {x:0.5, y:0, z:2.8},
-                rotate: true
-            },
-            n17: {
-                sz: {scale:0.002},
-                pos: {x:1, y:-0.64, z:1.0},
-                rotate: false,
-                ori: {x:0, y:Math.PI, z:0}
-            },
-            n10: {
-                sz: {scale: 0.01},
-                pos: {x:0, y:4, z:5},
-                rotate: true
-            },
-            s1: {
-                sz: {scale: 0.02},
-                pos: {x:0, y:0, z:5},
-                ori: {x:0, y:1.62, z:0}
-                // rotate: true
-            },
-            ql1: {
-                sz: {scale: 0.02},
-                pos: {x:0, y:0, z:5},
-                ori: {x:0, y:Math.PI, z:0}                
-            },
-            nym1: {
-                sz: {scale: 0.13},
-                pos: {x:0, y:3, z:5},
-                ori: {x:0, y:Math.PI, z:0}                
-            },
-            
-            default: {
-                sz: {scale: 0.5},
-                pos: {x:0, y:1, z:5},
-                rotate: true
-            }
-            
-        }
-        
-        // var configs = {};
         var addCfg = function (label, path, params) {
             scene.objConfigs[label] = {label: label, path: path, params: params};
         }
+        
+        
+        // if (myCfg.rotate) {
+        //     mdl.behaviours.push(function (drawable, timePoint) {
+        //         drawable.currentOrientation = {x:0.0, y:Math.PI*2*(timePoint/7000), z:0.0};
+        //     });
+        // }
+        
         
         addCfg('nymph_in_shell', 'obj/nymph_in_shell.obj', {scale:0.11, y:2.25, z:3.5, ry:180/DEG, textureLabel:'concrete01'});
         addCfg('android-bust', 'obj/bust_of_android_girl.obj', {scale: 0.0016, y: 1.3});
@@ -305,60 +214,7 @@ window.ExperimentalScene = (function () {
         addCfg('nude-reclining', 'obj/nude_reclining.obj', {scale: 0.002, y:-0.65, z:1.8, ry:180/DEG});
         addCfg('nude-kneeling', 'obj/kneeling.obj', {scale:0.013, ry:2.1/RAD});
         addCfg('nude-almost', 'obj/almost_nude.obj', {scale:0.01, ry:2.9});
-        
-        OBJ.downloadMeshes({
-            // n1: 'almost_nude.obj', // 21mb, good but incomplete +
-            // n10: 'bust_of_android_girl.obj', // 2.4mb and geniunely fucking excellent
-            // n14a: 'nude_woman__alt9.obj', //
-            // n17: 'sexy_girl__alt3.obj',
-            // n3: 'woman.obj',
-            // bg1: 'beach_girl.obj',
-            // n5: 'kneeling.obj', // 21.3mb, good but incomplete +
-            // n14: 'nude_woman.obj', // 17.7mb partial, no normals ~
-            // n16: 'Talia.obj', //1.4mb good but no normals ~
-            // nym1: 'nymph_in_shell_hires.obj',
-            // s1: 'satyr.obj',
-            // ql1: 'qilinsongbao.obj',
-            body1: 'body.obj',
-            // m1: 'suzanne.obj' // 79kb monkey
-
-            // n1: 'almost_nude__alt2.obj', // 21mb, good but incomplete +
-            // n9: 'Dangerous_girl.obj', // 2.7mb, really good ++
-            // n10: 'bust_of_android_girl__alt2.obj', // 2.4mb and geniunely fucking excellent
-            // n14a: 'nude_woman.obj', //
-            // n17: 'sexy_girl.obj',
-            // n3: 'woman__alt4.obj',
-            // n3n: 'woman__alt_n3.obj',
-            // n4: 'girl_in_stockings.obj', // 2.4mb, good ++
-            // n5: 'kneeling__alt2.obj', // 21.3mb, good but incomplete +
-
-            // n6: 'pregnant.obj', // 17mb, good but incomplete +
-            // n7: 'nude_art.obj', // 29mb freakout --
-            // n8: 'girl.obj', // 56mb freakout --
-            // n11: 'woman_body.obj', // 5.9mb partial dismembered -
-            // n12: 'nude_on_stand.obj', // 6.4mb set limit to 110310 for a point cloud
-            // n13: 'photoshoot.obj', // 13mb trainwreck --
-            // n15: 'girl_2.obj', //24mb nup --
-        }, function (meshes) {
-            var which = 'body1';
-            console.debug(meshes);
-            window.MESHES = meshes;
-            var myMesh = meshes[which];
-            var myCfg = cfgs[which] || cfgs.default;
-            window.MESHLIMIT = myCfg.meshLimit || Infinity;
-            OBJ.initMeshBuffers(scene.gl, myMesh);
-            var mdl = new LoaderObj(myMesh, myCfg.pos, myCfg.sz, myCfg.ori, {label:'sculpture', shaderLabel: 'diffuse', textureLabel:'forestgreen'});
-            window.MDL = mdl;
-            if (myCfg.rotate) {
-                mdl.behaviours.push(function (drawable, timePoint) {
-                    drawable.currentOrientation = {x:0.0, y:Math.PI*2*(timePoint/7000), z:0.0};
-                });
-            }
-            // window.MESHLIMIT=0;
-            // window.setInterval(function (){window.MESHLIMIT+=3000;}, 50);
-            scene.addObject(mdl);
-        })
-        
+                
         /* Floor */
         scene.addObject(new FCShapes.WallShape(
             {x: 0, z: 0, y: -0.02},
@@ -400,10 +256,10 @@ window.ExperimentalScene = (function () {
                     scene.latest.scaleFactor *= 0.995;
                 }
                 else if (sector == 'w') {
-                    scene.latest.orientation.y += 0.2/DEG;
+                    scene.latest.orientation.y += 0.6/DEG;
                 }
                 else if (sector == 'e') {
-                    scene.latest.orientation.y -= 0.2/DEG;
+                    scene.latest.orientation.y -= 0.6/DEG;
                 }
             }
             // console.log(btnIdx, btnStatus, sector);
